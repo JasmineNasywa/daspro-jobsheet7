9b6f1d7724e594c4a56b76787d7ef8a213e53910
JasmineNasywa <jasminenasywa6@gmail.com>
2024-10-10 14:38:38 +0700

    percobaan 1 no4

--- siakadfor12.java
+++ siakadfor12.java
@@ -3,7 +3,8 @@ public class siakadfor12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-double nilai, tertinggi = 0, terendah = 100; {
+double nilai, tertinggi = 0, terendah = 100;
+int jmllulus = 0, jmltdklulus = 0; {
 
 for (int i = 1; i <= 10; i++) {
     System.out.println("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -13,10 +14,23 @@ for (int i = 1; i <= 10; i++) {
     }
     if (nilai < terendah) {
         terendah = nilai;
+    } 
+    
+    
+    
+    if (nilai <= 60) {
+        System.out.println("Mahasiswa tidak lulus");
+        jmltdklulus++;
+    } else {
+        System.out.println("Mahasiswa lulus");
+        jmllulus++;
     }
+
 }
  System.out.println("Nilai tertinggi = " + tertinggi);
  System.out.println("Nilai terendah = " + terendah);
+ System.out.println("Jumlah mahasiswa lulus = " + jmllulus);
+ System.out.println("Jumlah mahasiswa tidak lulus = " + jmltdklulus);
     
 } }
     }
